fae612d6d9f418954ffa24a7e3d5ddc7dcfc2c53
Faithful-Mind <lpyy15@gmail.com>
2020-07-23 13:08:22 +0800

    First React unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,24 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+beforeAll(() => {
+  Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: jest.fn(), // deprecated
+      removeListener: jest.fn(), // deprecated
+      addEventListener: jest.fn(),
+      removeEventListener: jest.fn(),
+      dispatchEvent: jest.fn(),
+    })),
+  });
+});
+
+test('renders todo input box', () => {
+  const { getByPlaceholderText } = render(<App />);
+  const InputElement = getByPlaceholderText(/Thing to do/i);
+  expect(InputElement).toBeInTheDocument();
 });
