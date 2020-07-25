c5687149a3e965a3dff744063947b197b7af4ee5
Faithful-Mind <lpyy15@gmail.com>
2020-07-26 01:15:56 +0800

    Import first React Hooks

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Row, Col, Button } from 'antd';
 import './todo-item.css';
 
@@ -10,77 +10,60 @@ type TodoProps = {
   handleEdit: (id: string, value: string) => void;
 } & Todo;
 
-type State = { isEditing: boolean, newContent?: string }
-
-class TodoItem extends Component<TodoProps, State> {
-  constructor(props: Readonly<TodoProps>) {
-    super(props);
-    this.state = { isEditing: false };
-    this.handleEditClick = this.handleEditClick.bind(this);
-    this.handleEditOk = this.handleEditOk.bind(this);
-    this.handleEditing = this.handleEditing.bind(this);
+function TodoItem({
+  id, content, isDone, handleClickDone, handleRemove, handleEdit,
+}: TodoProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newContent, setNewContent] = useState('');
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+    setNewContent(content);
+  }
+  function handleEditOk() {
+    if (newContent) handleEdit(id, newContent);
+    setIsEditing(!isEditing);
+    setNewContent('');
+  }
+  function handleEditing(value: string) {
+    setNewContent(value);
   }
 
-  handleEditClick() {
-    this.setState((prevState) => ({
-      isEditing: !prevState.isEditing,
-      newContent: this.props.content,
-    }));
-  }
-
-  handleEditOk() {
-    if (this.state.newContent) this.props.handleEdit(this.props.id, this.state.newContent);
-    this.setState((prevState) => ({
-      isEditing: !prevState.isEditing,
-      newContent: undefined,
-    }));
-  }
-
-  handleEditing(value: string) {
-    this.setState({ newContent: value });
-  }
-
-  render() {
-    const {
-      id, content, isDone, handleClickDone, handleRemove,
-    } = this.props;
-    if (!isDone) {
-      if (!this.state.isEditing) {
-        return (
-          <Row>
-            <Col span={18} onClick={() => handleClickDone(id)}>
-              {content}
-            </Col>
-            <Col span={2}>
-              <Button onClick={this.handleEditClick}>Edit</Button>
-            </Col>
-            <Col span={4}>
-              <Button onClick={() => handleRemove(id)}>-</Button>
-            </Col>
-          </Row>
-        );
-      }
+  if (!isDone) {
+    if (!isEditing) {
       return (
-        // layouts for editing
-        <TodoEdit
-          newContent={this.state.newContent}
-          handleEditing={this.handleEditing}
-          handleEditOk={this.handleEditOk}
-        />
+        <Row>
+          <Col span={18} onClick={() => handleClickDone(id)}>
+            {content}
+          </Col>
+          <Col span={2}>
+            <Button onClick={handleEditClick}>Edit</Button>
+          </Col>
+          <Col span={4}>
+            <Button onClick={() => handleRemove(id)}>-</Button>
+          </Col>
+        </Row>
       );
     }
     return (
-      // layouts for done item
-      <Row>
-        <Col span={20} className="done" onClick={() => handleClickDone(id)}>
-          {content}
-        </Col>
-        <Col span={4}>
-          <Button onClick={() => handleRemove(id)}>-</Button>
-        </Col>
-      </Row>
+      // layouts for editing
+      <TodoEdit
+        newContent={newContent}
+        handleEditing={handleEditing}
+        handleEditOk={handleEditOk}
+      />
     );
   }
+  return (
+    // layouts for done item
+    <Row>
+      <Col span={20} className="done" onClick={() => handleClickDone(id)}>
+        {content}
+      </Col>
+      <Col span={4}>
+        <Button onClick={() => handleRemove(id)}>-</Button>
+      </Col>
+    </Row>
+  );
 }
 
 export interface Todo {
